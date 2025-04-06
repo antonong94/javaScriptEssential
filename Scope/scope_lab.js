6dc3e1d933429aee5ad40e786627164bb323711f
antonong94 <antonong94@gmail..com>
2025-04-06 00:23:08 -0400

    feat: controflow

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -6,27 +6,17 @@ const globalConst = "I'm a global constant";
 
 {
 // Block scope
-var blockVar = "I'm a block-scoped var";
-let blockLet = "I'm a block-scoped let";
-const blockConst = "I'm a block-scoped const";
+var blockVar = "block var"
+var blockVar = "new block var"
+let blockLet = "let"
+blockLet = "new blockLEt"
+const block = "abc"
 }
+block = "fff"
+blockVar = "outside blockVar"
+blockLet = "outside"
 
-// Global scope
-console.log(globalVar); // Output: "I'm a global variable"
-console.log(globalLet); // Output: "I'm also global, but scoped with let"
-console.log(globalConst); // Output: "I'm a global constant"
-
-//Block Scope
-// console.log(blockVar);
-// console.log(blockLet);
-
-function show(){
-var functionVar = "I'm a block-scoped var";
-let functionLet = "I'm a block-scoped let";
-const functionConst = "I'm a block-scoped const";
-}
-show();
-
-console.log(functionVar); // Throws ReferenceError
-console.log(functionLet); // Throws ReferenceError
-console.log(functionConst); // Throws ReferenceError
+console.log(globalConst)
+console.log(blockVar)
+console.log(blockLet)
+console.log(block)
